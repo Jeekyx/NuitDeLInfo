18e691443c6f1a54f554a68fe2883faf54ba98c7
mrollin <matthieu.rollin@gmail.com>
2014-12-04 20:58:53 +0100

    Base of the application with the start of the backend

---
+++ android/app/src/main/java/com/epita/mti/nuitdelinfoandroid/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.epita.mti.nuitdelinfoandroid.controller;
+
+import com.epita.mti.nuitdelinfoandroid.model.Model;
+
+/**
+ * Created by _Gary_ on 28/11/2014.
+ * Abstract Controller
+ */
+public abstract class Controller<T extends Model> {
+    /**
+     * The TAG for logs
+     */
+    private static final String TAG = Controller.class.getSimpleName();
+}
